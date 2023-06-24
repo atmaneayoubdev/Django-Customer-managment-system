08529b4b1a4193832f7f636a2d4ba712d1da832b
AtmaneDev23 <ayoubatmane23@gmail.com>
2023-06-25 00:47:17 +0400

    finished project

---
+++ testdb.py
@@ -0,0 +1,14 @@
+# import pymysql
+
+# try:
+#     conn = pymysql.connect(
+#         host='localhost',
+#         port=3306,
+#         user='root',
+#         password='kaliotman',
+#         database='dcrm',
+#     )
+#     print('Connection successful!')
+#     conn.close()
+# except Exception as e:
+#     print('Connection failed:', str(e))
